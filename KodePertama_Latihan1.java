91eb0a34c86c493fb3645fe325a7621822ea4b9a
yihahaha16 <falihaimtinan@gmail.com>
2026-02-05 14:01:31 +0700

    Create KodePertama_Latihan1.java

---
+++ KodePertama_Latihan1.java
@@ -0,0 +1,12 @@
+/*Nama: Faliha Imtinan
+NIM: D1041241004
+Hari/Tanggal: Kami, 5 Januari 2025
+*/
+public class KodePertama_Latihan1{
+    public static void main(String[] args){
+        System.out.println("Nama: Faliha Imtinan");
+        System.out.println("NIM: D1041241004");
+        System.out.println("Semester: 4");
+        System.out.println("Prodi: Informatika");
+    }
+}
