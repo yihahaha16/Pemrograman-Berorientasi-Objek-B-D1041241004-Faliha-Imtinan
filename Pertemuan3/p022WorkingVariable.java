022b74d3f64fdc8135ccd012eca69e944da6586c
yihahaha16 <falihaimtinan@gmail.com>
2026-02-12 14:02:32 +0700

    Pertemuan3

---
+++ Pertemuan3/p022WorkingVariable.java
@@ -0,0 +1,15 @@
+/*
+NIM: D1041241004
+Nama: Faliha Imtinan
+p022WorkingVariable
+*/
+package Pertemuan3;
+
+public class p022WorkingVariable{
+    public static void main(String[] args) {
+        int angka = 10;
+        System.out.println("Nilai awal angka: " + angka);
+        angka = 25;
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+}
---
+++ Pertemuan3/p023PrimitiveData.java
@@ -0,0 +1,23 @@
+/*
+NIM: D1041241004
+Nama: Faliha Imtinan
+p023PrimitiveData
+*/
+package Pertemuan3;
+
+public class p023PrimitiveData {
+    public static void main(String[] args) {
+        int umur =20;
+        long populasi = 8000000000L;
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+        char nilaiHuruf = 'A';
+        boolean lulus = true;
+        System.out.println("Umur            :" + umur);
+        System.out.println("Populasi        :" + populasi);
+        System.out.println("Tinggi Badan    :" + tinggi);
+        System.out.println("IPK             :" + ipk);
+        System.out.println("Nilai Huruf     :" + nilaiHuruf);
+        System.out.println("Status Lulus    :" + lulus);
+    }
+}
