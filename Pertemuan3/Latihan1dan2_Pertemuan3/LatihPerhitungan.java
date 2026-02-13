ed60b97d7af4c992db9629ce564172e01b7875a4
yihahaha16 <falihaimtinan@gmail.com>
2026-02-13 08:23:05 +0700

    LatihPerhitungan
    Latihan 2 Pertemuan 3

--- Pertemuan3/Latihan1dan2_Pertemuan3/LatihPerhitungan.java
+++ Pertemuan3/Latihan1dan2_Pertemuan3/LatihPerhitungan.java
@@ -1,3 +1,8 @@
+/*
+NIM: D1041241004
+Nama: Faliha Imtinan
+p022WorkingVariable
+*/
 package Pertemuan3.Latihan1dan2_Pertemuan3;
 
 public class LatihPerhitungan {
@@ -7,9 +12,11 @@ public class LatihPerhitungan {
         System.out.println("=== SIMULASI TRANSAKSI BANK ===");
         System.out.println("Saldo Awal            : "+SaldoAwal);
         System.out.println("Setoran               : "+Setoran);
+        //menambah saldo awal dengan setoran
         SaldoAwal += Setoran;
         System.out.println("Saldo Setelah Setor   : "+SaldoAwal);
         int Penarikan  = 250000;
+        //mengurangi saldo awal dan penarikan
         int SaldoAkhir = SaldoAwal - Penarikan;
         System.out.println("Penarikan             : " + Penarikan);
         System.out.println("Saldo Akhir           : " + SaldoAkhir);
