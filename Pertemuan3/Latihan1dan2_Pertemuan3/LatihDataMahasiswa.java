4a8ee25480c708a9412699b1cc86906f53137d60
yihahaha16 <falihaimtinan@gmail.com>
2026-02-13 08:15:52 +0700

    LatihDataMahasiswa
    Latihan 1 Pertemuan 3

---
+++ Pertemuan3/Latihan1dan2_Pertemuan3/LatihDataMahasiswa.java
@@ -0,0 +1,24 @@
+/*
+NIM: D1041241004
+Nama: Faliha Imtinan
+p023PrimitiveData
+*/
+package Pertemuan3.Latihan1dan2_Pertemuan3;
+
+public class LatihDataMahasiswa{
+    public static void main(String[] args){
+        int Nim = 231010001;
+        String Nama = "Budi Santoso";
+        char kelas='A';
+        boolean status_aktif = true;
+        double ipk = 3.75;
+        short Total_SKS = 72;
+        System.out.println("=== DATA MAHASISWA ===");
+        System.out.println("NIM             :" + Nim);
+        System.out.println("Nama            : " + Nama);
+        System.out.println("Kelas           : " + kelas);
+        System.out.println("Status Aktif    : " + status_aktif);
+        System.out.println("IPK Semester    :" + ipk);
+        System.out.println("Total SKS       : " + Total_SKS);
+    }
+}
